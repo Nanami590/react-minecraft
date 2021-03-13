c70ce224e2edce6047f29461a79a083f345bb401
Nanami590 <roblewandowski590@gmail.com>
2021-03-13 17:26:35 +0300

    add hovering to cube

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useBox } from "use-cannon";
 import * as textures from "../textures"; 
 
 
 export const Cube = ({position, type, ...props}) => {
+    const [hover, setHover] = useState(null);
+
     const [ref] = useBox(() => ({
         type: "Static",
         position,
         ...props
     }));
 
+    const toggleCubeHover = (event) => {
+        event.stopPropagation();
+        
+        setHover(Math.floor(event.faceIndex / 2));
+    };
+
     return (
-        <mesh castShadow ref={ref}>
+        <mesh castShadow ref={ref} onPointerMove={toggleCubeHover}>
             {[...Array(6)].map((_, index) => (
                 <meshStandardMaterial
                     key={index}
                     map={textures[type]}
                     attachArray={"material"}
+                    color={hover === index ? "gray" : "white"}
                 />
             ))}
             <boxBufferGeometry attach="geometry" />
